221fd1ab1a4a52f8eb2e73a77712a0798bca414e
ivandiaz2425 <ivandiaz2422@gmail.com>
2017-08-29 13:34:43 -0500

    Uno

---
+++ app/src/main/java/com/example/aula_7/solucion_quiz/Pantalla_2.java
@@ -0,0 +1,13 @@
+package com.example.aula_7.solucion_quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_2);
+    }
+}
